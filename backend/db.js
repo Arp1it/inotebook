b166ca5486b14f939f7a4c58dc0721639053544e
Arpit <arpitjaiswal9526@gmail.com>
2024-10-03 20:23:22 +0530

    Connecting to mongodb compass

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = () => {
+  mongoose.connect('mongodb://127.0.0.1:27017/rec')
+    .then(() => {
+      console.log('Connected to MongoDB:', mongoose.connection.readyState); // 1 indicates connected
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+};
+
+module.exports = connectToMongo;
