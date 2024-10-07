3d3bc8256eba4adaaf18f2d478536cb7f74ddc85
Arpit <arpitjaiswal9526@gmail.com>
2024-10-07 18:42:47 +0530

    Storing user data in mongodb

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/User');
 
 
-router.get("/", (req, res)=>{
-    obj = {
-        a: 'Thanos',
-        Number: 420
-    }
+// Create a user using Post "/api/auth". Dosn't required auth
+router.post("/", (req, res)=>{
 
-    res.json(obj)
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.json(req.body)
+
 })
 
 module.exports = router
