f35a979105988923a96a11156fe537ab4520499b
Arpit <arpitjaiswal9526@gmail.com>
2024-10-08 09:21:57 +0530

    Adding validation while storing data

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = () => {
-  mongoose.connect('mongodb://127.0.0.1:27017/rec')
+  mongoose.connect('mongodb://127.0.0.1:27017/inotebook')
     .then(() => {
       console.log('Connected to MongoDB:', mongoose.connection.readyState); // 1 indicates connected
     })
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
 // Create a user using Post "/api/auth". Dosn't required auth
-router.post("/", (req, res)=>{
+router.post("/", [
+    body('name', "username length must be at least 3 character").isLength({min:3}),
+    body('email', "enter a valid email").isEmail(),
+    body('password', "password length must be at least 5 chars").isLength({min:5})
+], (req, res)=>{
 
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.json(req.body)
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      
+      return User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }).then(user=>res.json(user))
+      .catch(err => res.status(500).json({error: "Please enter a unique value for email", err}))
+
+    }
+  
+    res.status(400).json({ errors: result.array() });
 
 })
 
