90f91778ec6d0adeab834bf4bec542dd1204af4a
Arpit <arpitjaiswal9526@gmail.com>
2025-04-27 18:47:05 +0530

    Update Notes feature added

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -42,4 +42,33 @@ router.post("/addnote", fetchuser, [
     }
 })
 
+// Route 3: Update an existing note using PUT "/api/notes/updatenote". Login required
+router.post("/updatenote/:id", fetchuser, async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+        // Create a new note object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        // Find the note to be updated and update it
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+        if (note.user.toString() !== req.user.id){
+            return res.status(401).send("Not Allowed")
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new:true });
+        res.json({ note });
+
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+    
+
+})
+
 module.exports = router
